e10183222967b6f48cb5dcae676edae44042b342
alexander <alex.schacher@gmail.com>
2024-10-01 14:27:14 +0200

    :construction: added first route

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,31 @@
+
+val kotlin_version: String by project
+val logback_version: String by project
+
 plugins {
     kotlin("jvm") version "2.0.20"
+    id("io.ktor.plugin") version "3.0.0-rc-1"
 }
 
-group = "pro.schacher"
-version = "1.0-SNAPSHOT"
+group = "com.example"
+version = "0.0.1"
+
+application {
+    mainClass.set("io.ktor.server.netty.EngineMain")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-kotlin {
-    jvmToolchain(17)
+    implementation("io.ktor:ktor-server-core-jvm")
+    implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.ktor:ktor-server-config-yaml")
+    testImplementation("io.ktor:ktor-server-test-host-jvm")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
---
+++ src/main/kotlin/com/example/Application.kt
@@ -0,0 +1,13 @@
+package com.example
+
+import com.example.plugins.*
+import io.ktor.server.application.*
+import io.ktor.server.netty.*
+
+fun main(args: Array<String>) {
+    EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureRouting()
+}

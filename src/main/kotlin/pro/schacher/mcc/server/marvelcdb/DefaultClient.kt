455390d996a2b343f79843a6f89baf97eb9e0c99
alexander <alex.schacher@gmail.com>
2025-02-22 21:02:54 +0100

    :twisted_rightwards_arrows: fixed merge conflicts

--- src/main/kotlin/pro/schacher/mcc/server/marvelcdb/DefaultClient.kt
+++ src/main/kotlin/pro/schacher/mcc/server/marvelcdb/DefaultClient.kt
@@ -4,7 +4,6 @@ import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.HttpResponseValidator
 import io.ktor.client.plugins.cache.HttpCache
-import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
@@ -13,33 +12,33 @@ import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 
 val DefaultClient = HttpClient(CIO) {
-        followRedirects = true
-        install(HttpCache) {}
-        install(ContentNegotiation) {
-            json(Json {
-                ignoreUnknownKeys = true
-                explicitNulls = false
-            })
-        }
-        install(Logging) {
-            logger = object : Logger {
-                override fun log(message: String) {
-                    println(message)
-                }
+    followRedirects = true
+    install(HttpCache) {}
+    install(io.ktor.client.plugins.contentnegotiation.ContentNegotiation) {
+        json(Json {
+            ignoreUnknownKeys = true
+            explicitNulls = false
+        })
+    }
+    install(Logging) {
+        logger = object : Logger {
+            override fun log(message: String) {
+                println(message)
             }
-            level = LogLevel.INFO
         }
+        level = LogLevel.INFO
+    }
 
-        HttpResponseValidator {
-            validateResponse { response ->
-                when (response.status) {
-                    HttpStatusCode.OK -> return@validateResponse
-                    HttpStatusCode.Unauthorized -> throw AuthorizationException()
-                    else -> throw RemoteServiceException(
-                        statusCode = response.status,
-                        message = "Remote service returned status code ${response.status}"
-                    )
-                }
+    HttpResponseValidator {
+        validateResponse { response ->
+            when (response.status) {
+                HttpStatusCode.OK -> return@validateResponse
+                HttpStatusCode.Unauthorized -> throw AuthorizationException()
+                else -> throw RemoteServiceException(
+                    statusCode = response.status,
+                    message = "Remote service returned status code ${response.status}"
+                )
             }
         }
     }
+}

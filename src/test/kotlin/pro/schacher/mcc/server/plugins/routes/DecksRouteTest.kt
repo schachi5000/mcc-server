f90fac8908792fbb6d5451b466a46c272ddbe954
alexander <alex.schacher@gmail.com>
2025-02-26 12:22:49 +0100

    :art: getSpotlight test is now converting to DeckDto

--- src/test/kotlin/pro/schacher/mcc/server/plugins/routes/DecksRouteTest.kt
+++ src/test/kotlin/pro/schacher/mcc/server/plugins/routes/DecksRouteTest.kt
@@ -6,7 +6,7 @@ import io.ktor.http.HttpStatusCode
 import io.ktor.server.testing.testApplication
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
-import pro.schacher.mcc.server.dto.PackDto
+import pro.schacher.mcc.server.dto.DeckDto
 import pro.schacher.mcc.server.module
 import pro.schacher.mcc.server.toDto
 import kotlin.test.Test
@@ -18,9 +18,9 @@ class DecksRouteTest {
             module()
         }
 
-        val response = this.client.get("/api/v1/decks/spotlight?date=2025-01-01")
-        val packs = response.bodyAsText().toDto<List<PackDto>>()
+        val response = this.client.get("/api/v1/decks/spotlight?date=2025-02-26")
+        val decks = response.bodyAsText().toDto<List<DeckDto>>()
         assertEquals(HttpStatusCode.OK, response.status)
-        assertTrue(packs.isNotEmpty())
+        assertTrue(decks.isNotEmpty())
     }
 }
